941fbdfefd98947828604741411376323c4f17b8
kamal raj <kamaljp@gmail.com>
2022-09-27 07:01:12 +0530

    Completed ML5 ID me with ML5

--- ML/ch1/main.js
+++ ML/ch1/main.js
@@ -1,12 +1,43 @@
-var input;
+const { text } = require("express");
+
+var mobilenet;
+var arcticFox;
+var loadRes;
+var label;
+
+function getClassifier(){
+    console.log('Model Ready...')
+    mobilenet.predict(arcticFox,getResults);
+}
+
+function getResults(error, prediction){
+    if(error){
+        console.log(error)
+    } else {
+        const outData = prediction[0].label.split(',');
+        const outText = outData.join(' or a')
+        console.log(outText)
+        var confidence = prediction[0].confidence * 100;
+        confidence = confidence.toFixed(2)
+        loadRes.html(`The image is ${outText} with a confidence of ${confidence}%.`)
+
+        //Writing on image itself
+        label = `It is a picture of ${prediction[0].label.split(',')[0]}`;    }
+
+    }   
+
+function getImage(){
+    image(arcticFox,0,0,width,height)
+    //loadImg.html(arcticFox)
+}
 
 function setup(){
-    noCanvas();
-    input = createInput();
-    input.changed(newText)
-}
+    createCanvas(450,450)
+    background(57)
 
-function newText(){
-    console.log(input.value())
-}
+    mobilenet = ml5.imageClassifier('MobileNet',getClassifier)
+    arcticFox = createImg('arcticFox.jpg',getImage)
+    arcticFox.hide()
 
+    loadRes= select("#resOut")
+}
