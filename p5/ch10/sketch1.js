620150db5212a8a7dc5009381826b51ab9b23eb9
kamal raj <kamaljp@gmail.com>
2022-09-30 19:09:37 +0530

    Started Using SelectAll

---
+++ p5/ch10/sketch1.js
@@ -0,0 +1,102 @@
+var chg;
+var remo;
+var heads;
+var paras;
+var supa;
+function setup(){
+    noCanvas()
+    chg = select('#changer')
+    remo = select('#remover')
+    supa = select('#superior')
+
+    heads = selectAll('h1')
+    paras = selectAll('p')
+
+    chg.mousePressed(blueGreen)
+    chg.mouseReleased(greenOrange)
+
+    supa.mousePressed(styleBg)
+    supa.mouseReleased(styleGo)
+
+    remo.mouseClicked(removeMagic)
+    console.log(heads)
+
+}
+
+function styleBg(){
+    for (let head of heads){
+        if (head.class()){
+            var hClass = head.class()
+            head.removeClass(hClass)
+            head.addClass('blue')
+        }
+        head.addClass('blue')
+    }
+    for (let pa of paras){
+        pa.style('color','green')
+    }
+} 
+
+function styleGo(){
+    for (let head of heads){
+        if (head.class()){
+            var hClass = head.class()
+            head.removeClass(hClass)
+            head.addClass('green')
+        }
+        head.addClass('green')
+    }
+    for (let pa of paras){
+        pa.style('color','orange')
+    }
+}
+
+
+function blueGreen(){
+    for (let head of heads){
+        if (head.class()){
+            var hClass = head.class()
+            head.removeClass(hClass)
+            head.addClass('blue')
+        }
+        head.addClass('blue')
+    }
+    for (let pa of paras){
+        if(pa.class()){
+            var pClass = pa.class()
+            pa.removeClass(pClass)            
+            pa.addClass('green')
+        }
+        pa.addClass('green')
+    }
+} 
+
+function greenOrange(){
+    for (let head of heads){
+        if (head.class()){
+            var hClass = head.class()
+            head.removeClass(hClass)
+            head.addClass('green')
+        }
+        head.addClass('green')
+    }
+    for (let pa of paras){
+        if(pa.class()){
+            var pClass = pa.class()
+            pa.removeClass(pClass)
+            pa.addClass('orange')
+        }
+        pa.addClass('orange')
+    }
+}   
+
+function removeMagic(){
+    for (let head of heads){
+        var headClass = head.class()
+        head.removeClass(headClass)
+    }
+    
+    for (let pa of paras){
+        pa.style('color','black')
+    }
+}
