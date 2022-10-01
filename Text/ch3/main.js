01f59a92684095a63545f2ac21ead72d1b815489
Kamalraj M M <kamaljp@gmail.com>
2022-10-01 22:28:09 +0530

    regexin

--- Text/ch3/main.js
+++ Text/ch3/main.js
@@ -1,13 +1,57 @@
 var textGiven;
 var expression;
+var textOut
 
 var matcher;
 var tester;
+var execer;
 
 function setup(){
     noCanvas()
     textGiven = select('#textData');
-    expression = select('#expr')
+    expression = select('#expr');
+    textOut = select('#textOut')
 
+    matcher = select('#mat')
+    tester = select('#tes')
+    execer = select('#exe')
+
+    matcher.mouseClicked(matchExpr)
+    tester.mouseClicked(testExpr)
+    execer.mouseClicked(execExpr)
+}
+
+function testExpr(){
+    print(textGiven.value())
+    let locReg = new RegExp(expression.value())
+    let locRes = locReg.test(textGiven)
     
+    var resPara = createP(locRes)
+            .parent('textOut')
+}
+
+function matchExpr(){
+    print(textGiven.value())
+    console.log('got Sig')
+    let locReg = new RegExp(expression.value(), 'i')
+    
+    let locRes = textGiven.value().match(locReg)
+    
+    console.log(locRes)
+    
+    var resPara = createP(locRes)
+        .parent('textOut')
+}
+
+function execExpr(){
+
+    let locReg = new RegExp(expression.value(), 'i')
+    
+    while(true){    
+        let locRes = locReg.exec(textGiven.value());
+    print(locReg)
+    if(locRes){
+        createP(locRes[0])
+            .parent('textOut')
+    }
 }
